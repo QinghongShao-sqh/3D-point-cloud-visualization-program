22584bd0d52fa24d63ad78c0760444f3ca7fe604
qinghongshao <76911096+KEXA1@users.noreply.github.com>
2023-05-24 23:39:52 +0800

    Add files via upload

---
+++ Code/main.cpp
@@ -0,0 +1,14 @@
+﻿// by kk nuaa 
+#include "mainwindow.h"
+
+#include <QApplication>
+
+#include <vtkOutputWindow.h>
+int main(int argc, char *argv[])
+{
+	vtkOutputWindow::SetGlobalWarningDisplay(0);//不弹出vtkOutputWindow窗口
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
